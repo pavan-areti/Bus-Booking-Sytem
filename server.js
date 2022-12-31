decb0269956bae63012d71a1d4e76d651ad1031c
pavan <pavanareti02@gmail.com>
2022-12-31 16:00:37 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,9 @@
+// express server
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const dbConfig = require('./config/dbConfig');
+const port = process.env.PORT || 5000;
+
+//listen to port
+app.listen(port, () => console.log(`Listening on port ${port}`));
